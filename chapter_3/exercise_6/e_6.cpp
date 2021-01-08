4d9392c5cfd635a632648d4e602d60773d91cb29
javierpelayo <contact@javierperez.dev>
2021-01-07 16:50:49 -0800

    Add exercise 6

---
+++ chapter_3/exercise_6/e_6.cpp
@@ -0,0 +1,33 @@
+/*
+*   Takes two integers as input and determines the smaller, larger, sum,
+*   difference, product, and ratio of these values & outputs them.
+*   
+*   Book: Programming Principles and Practice Using C++ 2nd Edition
+*   Author: Bjarne Stroustrup
+*   Section: Chapter 3, Exercise 4
+*/
+#include<iostream>
+#include<string>
+
+int main(){
+    int val1 = 0;
+    int val2 = 0;
+    int val3 = 0;
+
+    std::cout << "Enter three integers separated by spaces:" << std::endl;
+    std::cin >> val1 >> val2 >> val3;
+
+    if(val1 <= val2 && val1 <= val3){
+        std::cout << val1 << " ";
+        std::cout << ((val2 <= val3) ? val2 : val3) << " ";
+        std::cout << ((val3 <= val2) ? val2 : val3) << std::endl;
+    } else if(val2 <= val1 && val2 <= val3){
+        std::cout << val2 << " ";
+        std::cout << ((val1 <= val3) ? val1 : val3) << " ";
+        std::cout << ((val3 <= val1) ? val1 : val3) << std::endl;
+    } else {
+        std::cout << val3 << " ";
+        std::cout << ((val2 <= val1) ? val2 : val1) << " ";
+        std::cout << ((val1 <= val2) ? val2 : val1) << std::endl;
+    }
+}
