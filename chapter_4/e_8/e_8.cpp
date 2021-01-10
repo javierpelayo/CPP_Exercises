c5553d19b0d29878b5343c96954d8ed9d6d51a48
javierpelayo <contact@javierperez.dev>
2021-01-10 01:51:21 -0800

    Update exercise 4.8

--- chapter_4/e_8/e_8.cpp
+++ chapter_4/e_8/e_8.cpp
@@ -1,5 +1,5 @@
 /*
-*   Calculates where the number of rice is 1 thousand, 1 million, and 1 billion
+*   Calculates where the number of rice is atleast 1 thousand, 1 million, and 1 billion
 *   on a chess board square, rice is doubled at each subsequent square.
 *   
 *   Book: Programming Principles and Practice Using C++ 2nd Edition
@@ -13,9 +13,9 @@
 #include<string>
 
 int main() {
-    int t = 1000;
-    int m = 1000000;
-    int b = 1000000000;
+    constexpr int t = 1000;
+    constexpr int m = 1000000;
+    constexpr int b = 1000000000;
 
     int rice_at = 1;
     int cur_square = 0;
@@ -23,7 +23,7 @@ int main() {
     while(cur_square <= 64){
         rice_at *= 2;
         if((rice_at >= t && rice_at <= t*2) || (rice_at >= m && rice_at <= m*2) || (rice_at >= b && rice_at <= b*2))
-            std::cout << rice_at << " grains of rice for the " << cur_square << " square." << std::endl;
+            std::cout << rice_at << " grains of rice for the " << cur_square << "th square." << std::endl;
         ++cur_square;
     }
     return 0;
