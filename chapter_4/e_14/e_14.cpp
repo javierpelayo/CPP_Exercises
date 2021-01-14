dcb02fd84d3f981cf7daf4049f8ebd8caacea6c9
javierpelayo <contact@javierperez.dev>
2021-01-13 20:30:44 -0800

    Sieve of Eratosthenes

--- chapter_4/e_14/e_14.cpp
+++ chapter_4/e_14/e_14.cpp
@@ -1,9 +1,9 @@
 /*
-*   Finds all the prime numbers between 1 and 100 using the "Sieve of Eratosthenes".
+*   Finds all the prime numbers between 1 and max using the "Sieve of Eratosthenes".
 *   
 *   Book: Programming Principles and Practice Using C++ 2nd Edition
 *   Author: Bjarne Stroustrup
-*   Section: Chapter 4, Exercise 13
+*   Section: Chapter 4, Exercise 14
 *
 *   Compile: g++ -std=c++11 name.cpp -o name
 */
@@ -11,8 +11,8 @@
 #include<vector>
 #include<time.h>
 
-void mark_comp(int k, std::vector<bool>& c) {
-    for (int j = (k*k); j < 100; j+=k) {
+void mark_comp(int k, int m, std::vector<bool>& c) {
+    for (int j = (k*k); j < m; j+=k) {
         c[j] = 1;
     }
 }
@@ -20,17 +20,20 @@ void mark_comp(int k, std::vector<bool>& c) {
 int main() {
     clock_t clkStart = clock();
     clock_t clkFinish;
-    int count = 0;
     std::vector<int> primes;
     std::vector<bool> comps(101);
+    int max = 0;
 
-    for(int i = 2; i*i < 100; ++i) {
+    std::cout << "Enter a max number to find primes:\n";
+    std::cin >> max;
+
+    for(int i = 2; i*i < max; ++i) {
         if(comps[i] == 0){
-            mark_comp(i, comps);
+            mark_comp(i, max, comps);
         }
     }
 
-    for (int k = 2; k < 100; ++k) {
+    for (int k = 2; k < max; ++k) {
         if(comps[k] == 0) {
             primes.push_back(k);
         }
