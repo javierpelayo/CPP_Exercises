5a64c6e654ef4a5d4a46f13d48e705ac987028d0
javierpelayo <contact@javierperez.dev>
2021-12-22 13:30:38 -0800

    chapter 10 exercise 2

--- chapter_10/e_2/e_2.cpp
+++ chapter_10/e_2/e_2.cpp
@@ -19,11 +19,16 @@ class Readings {
 };
 
 void write() {
+    char ch1;
+    char ch2;
     int day;
     int hour;
     double temp;
     std::vector<Readings> raw_temps;
-    while(std::cin >> day >> hour >> temp && day != -1) {
+    std::cout << "Enter temperature readings as: (day hour temp)\n";
+    std::cout << "Enter * to stop reading:\n";
+    while(std::cin >> ch1 && ch1 != '*') {
+        std::cin >> day >> hour >> temp >> ch2;
         Readings r;
         r.day = day;
         r.hour = hour;
@@ -40,28 +45,49 @@ void write() {
 }
 
 void read() {
+    std::cout << "Enter the name of the file you want to read with its extension:\n";
     std::string filename;
     std::cin >> filename;
     if(std::cin.fail()) {
         std::cout << "Something went wrong with input. Closing program.";
     } else {
         std::ifstream ifst{filename};
+
+        if(!ifst.is_open())
+            std::cout << "File was not opened.";
+
         char ch1;
         char ch2;
         int day;
         int hour;
         double temp;
-        std::vector<Readings> file_temps;
+        char terminator = '*';
+        int n = 0;
         while(ifst >> ch1 >> day >> hour >> temp >> ch2) {
-            std::cout << "(" << day << " " << hour << " " << temp << ")\n";
+            std::cout << "Line "<< n << ": (" << day << " " << hour << " " << temp << ")\n";
+            n++;
         }
+
+        if(ifst.fail()) {
+            ifst.clear();
+            char c;
+            ifst >> c;
+            
+            if(c != terminator) {
+                ifst.unget();
+                ifst.clear(std::ios_base::failbit);
+                std::cout << "Something went wrong: fail state\n";
+            }
+
+        } else if (ifst.bad()) {
+            std::cout << "Something went wrong: bad state\n";
+        }
+
     }
 }
 
 int main() {
     int input;
-    std::string filename = "raw_temps.txt";
-    std::ofstream ofst{filename};
 
     std::cout << "==Temperature Readings==\n";
     std::cout << "Would you like to 1-read or 2-write to a file?(type 1 or 2):\n";
@@ -70,10 +96,8 @@ int main() {
         std::cout << "That wasn't an integer.\n Closing program.";
         return 0;
     } else if(input == 1) {
-        std::cout << "Enter the name of the file you want to read with its extension:\n";
         read();
     } else if(input == 2) {
-        std::cout << "Enter temperature readings as: day hour temp :\n";
         write();
     }
     return 0;
